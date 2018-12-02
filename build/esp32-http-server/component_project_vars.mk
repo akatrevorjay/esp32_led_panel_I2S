8b496fd4ab605f005325f02be6a96e5cf2874789
Trevor Joynson (trevorj) <github@skywww.net>
2018-12-01 22:13:06 -0800

    remove build

--- build/esp32-http-server/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/main/esp32-http-server/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/esp32-http-server -lesp32-http-server
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += esp32-http-server
-component-esp32-http-server-build: 
